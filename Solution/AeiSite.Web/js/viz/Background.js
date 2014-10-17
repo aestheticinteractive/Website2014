efc6301a762465260efc964ddebfdfbc3afa8374
Zach Kinstner <zach@aestheticinteractive.com>
2014-10-17 13:23:42 -0400

    Improved the background-drawing process.

--- Solution/AeiSite.Web/js/viz/Background.js
+++ Solution/AeiSite.Web/js/viz/Background.js
@@ -7,9 +7,7 @@ Aei.Background = function(containerId) {
 		container: containerId
 	});
 
-	this._layerLinesA = new Kinetic.Layer();
-	this._layerLinesA.opacity(0.02);
-	this._stage.add(this._layerLinesA);
+	this._lineData = this._buildLineData();
 
 	var me = this;
 
@@ -17,79 +15,131 @@ Aei.Background = function(containerId) {
 		me._handleResize();
 	});
 
-	/*$(window).scroll(function(a, b, c) {
-		me._handleResize();
-	});*/
-
 	this._handleResize();
 };
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*----------------------------------------------------------------------------------------------------*/
-Aei.Background.prototype._handleResize = function() {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
-	//var scroll = $(window).scrollTop();
+Aei.Background.prototype._buildLineData = function() {
+	var data = {
+		width: 0,
+		height: 0,
+		groups: [
+			{
+				slope: -0.125,
+				opacity: 1
+			},
+			{
+				slope: -5,
+				opacity: 0.666
+			},
+			{
+				slope: 15,
+				opacity: 0.333
+			}
+		]
+	};
+	
+	var stage = this._stage;
+	var gapSize = 50;
+	var layerOpacity = 0.04;
+	var i, g;
 
-	if ( this._width == w && this._height == h /*&& this._scroll == scroll*/ ) {
+	for ( i = 0 ; i < data.groups.length ; ++i ) {
+		g = data.groups[i];
+		g.slopePos = (g.slope > 0);
+		g.gap = gapSize*(1+Math.abs(g.slope))*(g.slopePos ? -1 : 1);
+		g.lines = [];
+
+		g.layer = new Kinetic.Layer();
+		g.layer.opacity(layerOpacity);
+		stage.add(g.layer);
+	}
+
+	return data;
+};
+
+/*----------------------------------------------------------------------------------------------------*/
+Aei.Background.prototype._addLineData = function(width, height) {
+	var data = this._lineData;
+
+	if ( data.width == width && data.height == height ) {
 		return;
 	}
 
-	this._width = w;
-	this._height = h;
-	//this._scroll = scroll;
+	var i, g, offset, opac, px, lineI, lines, linesLen, line, isWithin;
 
-	this._layerLinesA.destroyChildren();
+	for ( i = 0 ; i < data.groups.length ; ++i ) {
+		g = data.groups[i];
+		offset = height*g.slope;
+		opac = g.opacity;
+		px = (g.slopePos ? width : 0);
+		lineI = 0;
+		lines = g.lines;
+		linesLen = lines.length;
 
-	var styles = [
-		{
-			slope: -0.125,
-			opacity: 1
-		},
-		{
-			slope: -5,
-			opacity: 0.75
-		},
-		{
-			slope: 15,
-			opacity: 0.5
-		}
-	];
+		g.layer.clip({
+			x: 0,
+			y: 0,
+			width: width,
+			height: height
+		});
 
-	var i, style, offset, opac, px, slopePos, gap;
+		while ( true ) {
+			isWithin = (g.slopePos ? px+offset > 0 : px+offset < width);
 
-	for ( i = 0 ; i < styles.length ; ++i ) {
-		style = styles[i];
-		offset = h*style.slope;
-		opac = style.opacity;
-		slopePos = (style.slope > 0);
-		gap = 30*(1+Math.abs(style.slope))*(slopePos ? -1 : 1);
-		px = (slopePos ? w : 0);
+			if ( !isWithin && lineI >= linesLen ) {
+				break;
+			}
 
-		while ( slopePos ? px+offset > 0 : px+offset < w ) {
-			var line = new Kinetic.Line({
-				points: [
-					px, /*-scroll*opac*opac*/0,
-					px+offset, /*-scroll*opac*opac*/+h
-				],
-				stroke: 'rgba(255, 255, 255, '+opac+')',
-				strokeWidth: 1 //4*(opac*0.75+0.25)
-			});
-			this._layerLinesA.add(line);
+			if ( lineI < linesLen ) {
+				line = lines[lineI];
+			}
+			else {
+				line = new Kinetic.Line({
+					stroke: 'rgba(255, 255, 255, '+opac+')',
+					strokeWidth: 1
+				});
+				g.layer.add(line);
+				lines[lineI] = line;
+			}
+			
+			line.points([
+				px, 0,
+				px+offset, height
+			]);
+			line.visible(isWithin);
 
-			px += gap;
+			px += g.gap;
+			++lineI;
 		}
 	}
+};
 
-	this._layerLinesA.clip({
-		x: 0,
-		y: 0,
-		width: w,
-		height: h
-	});
 
-	this._stage
-		.width(w)
-		.height(h);
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*----------------------------------------------------------------------------------------------------*/
+Aei.Background.prototype._handleResize = function() {
+	//var t0 = performance.now();
+	var stage = this._stage;
+	var sw = stage.width();
+	var sh = stage.height();
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	//var type = 'Resize';
+	this._addLineData(w, h);
+
+	if ( sw < w || sh < h ) {
+		stage
+			.width(Math.max(w, sw))
+			.height(Math.max(h, sh));
+	}
+	else {
+		stage.draw();
+		//type = 'Draw';
+	}
+
+	//var t1 = performance.now();
+	//$('#perform').html(type+': '+(t1-t0).toFixed(2)+' ms');
 };
