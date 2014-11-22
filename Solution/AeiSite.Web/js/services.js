42a363f13405b7ae9d58bcdf33b42ac3056209ad
Zach Kinstner <zach@aestheticinteractive.com>
2014-11-22 09:33:48 -0500

    Split the Services-specific directive into two reusable directives.

--- Solution/AeiSite.Web/js/services.js
+++ Solution/AeiSite.Web/js/services.js
@@ -105,27 +105,36 @@ Aei.Angular.directive('tagTrendGraph', function() {
 });
 
 /*----------------------------------------------------------------------------------------------------*/
-Aei.Angular.directive('servicesPageEntry', function() {
+Aei.Angular.directive('onDelay', function() {
 	return {
-		link: function() {
+		scope: {
+			delayMs: '=delayMs',
+			delayClass: '=delayClass'
+		},
+		link: function(scope, element) {
 			var onTimeout = function() {
-				$('table.service tbody tr td.fill')
-					.addClass('entry')
-					.children('i')
-						.addClass('entry');
-
-				$('a[data-scroll]').click(function() {
-					var id = $(this).attr('data-scroll');
-
-					var animDest = {
-						'scrollTop': $('#'+id).offset().top
-					};
-
-					$('body').animate(animDest, 1000);
-				});
+				$(element).addClass(scope.delayClass);
 			};
+			
+			setTimeout(onTimeout, (scope.delayMs || 1))
+		}
+	};
+});
 
-			setTimeout(onTimeout, 1);
+/*----------------------------------------------------------------------------------------------------*/
+Aei.Angular.directive('scrollTo', function() {
+	return {
+		scope: {
+			targetId: '=targetId'
+		},
+		link: function(scope, element) {
+			$(element).click(function() {
+				var animDest = {
+					scrollTop: $('#'+scope.targetId).offset().top
+				};
+				
+				$('body').animate(animDest, 1000);
+			});
 		}
 	};
 });
